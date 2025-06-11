b97ad77956cc81f223d899a1a934417c0069e469
Semidio <Semidio@users.noreply.github.com>
2025-06-11 21:40:08 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Semidio/CustomBucket:master" 
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
